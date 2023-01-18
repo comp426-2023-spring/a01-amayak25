2db9cf9ac2725da5b216e8184eedd8c8b6a4fd5b
Kirsten <amayak@email.unc.edu>
2023-01-17 20:10:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var argv = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || 3000
+const port = argv.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -47,7 +47,7 @@ res.end();
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-sever.listen(port, ()=> {
+server.listen(port, ()=> {
 console.log('Server listening on port ${port}`);
 });
 });
